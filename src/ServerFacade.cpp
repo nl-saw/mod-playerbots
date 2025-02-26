b7cb9fa11d1001ee3599fb3b1c6e01c801e65d27
SaW <swerkhoven@outlook.com>
2025-02-26 18:06:02 +0100

    Update ServerFacade.cpp

--- src/ServerFacade.cpp
+++ src/ServerFacade.cpp
@@ -2,7 +2,7 @@
  * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license, you may redistribute it
  * and/or modify it under version 2 of the License, or (at your option), any later version.
  */
-
+// TEst2
 #include "ServerFacade.h"
 
 #include "Playerbots.h"
